ad97f6f32a8c73f32e9aa9856d701b40e9bee670
n10008918 <n10008918@qut.edu.au>
2023-01-11 04:34:50 +1000

    cloned repo

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Create route and the landing page. User should be able to navigate across pages (no need for contents on other pages) show page re-rendered with new page</h1>
+    </div>
+  );
+}
+
+export default App;
